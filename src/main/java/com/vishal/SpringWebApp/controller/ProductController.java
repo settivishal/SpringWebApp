5372ec24d060b55fc3898b977ff6014c02a47961
settivishal <vishalsetti2000@gmail.com>
2025-05-10 19:21:12 -0400

    Implemented add products API

--- src/main/java/com/vishal/SpringWebApp/controller/ProductController.java
+++ src/main/java/com/vishal/SpringWebApp/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.vishal.SpringWebApp.controller;
 import com.vishal.SpringWebApp.model.Product;
 import com.vishal.SpringWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,13 +13,19 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
     }
 
-    @RequestMapping("/products/{prodId}")
+    @GetMapping("/products/{prodId}")
     public Product getProductById(@PathVariable int prodId) {
         return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public String addProduct(@RequestBody Product prod) {
+        service.addProduct(prod);
+        return "Product added!";
     }
 }
--- src/main/java/com/vishal/SpringWebApp/service/ProductService.java
+++ src/main/java/com/vishal/SpringWebApp/service/ProductService.java
@@ -3,16 +3,17 @@ package com.vishal.SpringWebApp.service;
 import com.vishal.SpringWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "Iphone", 50000),  // This is actually correct!
             new Product(102, "Canon Camera", 70000)
-    );
+    ));
 
     public List<Product> getProducts() {
         return products;
@@ -23,5 +24,9 @@ public class ProductService {
                 .filter(p -> p.getProdId() == prodId)
                 .findFirst()
                 .get();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
     }
 }
